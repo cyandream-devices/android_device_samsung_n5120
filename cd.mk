0b5573fc4b2443edaace3623877650b5bc70cfe9
yanniks <kontakt@yanniks.de>
2014-05-18 12:45:59 +0200

    initial CyanDream commit
    Change-Id: I1ea68e139cc2bf26964fb19a0586cb6c0b61e20c

--- cd.mk
+++ cd.mk
@@ -1,18 +1,18 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := n5120
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n5120/full_n5120.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n5120
-PRODUCT_NAME := cm_n5120
+PRODUCT_NAME := cd_n5120
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N5120
 PRODUCT_MANUFACTURER := samsung
